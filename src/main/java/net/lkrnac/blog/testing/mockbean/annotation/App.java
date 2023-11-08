376ec81a09366b48b927c6d6c204c12faefc4d1b
Alex Tsang <tsangale@amazon.com>
2023-11-08 15:09:37 -0800

    Testing 1p and injecting 3p

---
+++ src/main/java/net/lkrnac/blog/testing/mockbean/annotation/App.java
@@ -0,0 +1,15 @@
+package net.lkrnac.blog.testing.mockbean;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+        System.out.println(com.mycompany.app.App.add(1,2));
+        System.out.println( "Hello World!" );
+    }
+}
