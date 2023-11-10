907baf1bda32384b0fe1441c689f8b50ef5df4ce
Alex Tsang <tsangale@amazon.com>
2023-11-09 18:16:53 -0800

    Style and reactivated tests

--- src/test/java/net/lkrnac/blog/testing/mockbean/EncodeTest.java
+++ src/test/java/net/lkrnac/blog/testing/mockbean/EncodeTest.java
@@ -4,7 +4,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class EncodeTest {
-//    @Test
+    @Test
     public void TestEncode(){
         Assert.assertEquals(com.mycompany.app.App.Encode(), "Hello");
     }
